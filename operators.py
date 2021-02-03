ccb0ce7b2be99f9f9cc9cf1e7233904d24ee09c5
paulmwangi556 <pmwas8294@gmail.com>
2021-02-03 17:07:17 +0300

    second commit

---
+++ operators.py
@@ -0,0 +1,28 @@
+#Arithmetic operators
+x=34
+y=54
+print(x+y)
+print(x%y)
+print(x*y)
+print(x-y)
+
+#strings only work with +(concatenation)
+x="first"
+y="last"
+print("first"+"last")
+print("first"-"last")
+print("first"%"last")
+
+
+
+
+
+#relational operators
+a="5"
+b="6"
+print("a">"b")
+print("a"<"b")
+print("a"<="b")
+print("a"=="b")
+print("a">="b")
+print("a"!="b")
